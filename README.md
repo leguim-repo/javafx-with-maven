ed42383b6261a006e2488093a21096dcd246da26
mike <60325499+leguim-repo@users.noreply.github.com>
2020-07-31 18:46:29 +0200

    Intial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# javafx-with-maven
+
+Estructura lista para usar javafx con maven en intellij
+
+---
+by Mike  
+![Created in Barcelona](createdinbcn.png "Created in Barcelona")
---
+++ src/main/java/org/example/FormPrueba.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class FormPrueba {
+
+    @FXML
+    private void btPruebaClick() {
+        System.out.println("Has hecho click sobre el boton de prueba");
+    }
+}
